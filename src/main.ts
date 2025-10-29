1b7d0ddf6088265b4455f40b83dccf9d98a4fbbc
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-29 15:07:44 +0330

    feat:validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({
+    transform:true , whitelist:true ,
+    forbidNonWhitelisted:true,
+    transformOptions:{ enableImplicitConversion:true }
+    }));
 
   //define swagger stting
   const config = new DocumentBuilder().setTitle("Nest App").build()
