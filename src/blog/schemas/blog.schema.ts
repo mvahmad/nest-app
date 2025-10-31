a27d767c4ddb3735be39ee3ab7d26f5209a4ebbb
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-31 15:18:52 +0330

    fix: correct typo in blog schema and enable timestamps

--- src/blog/schemas/blog.schema.ts
+++ src/blog/schemas/blog.schema.ts
@@ -1,10 +1,10 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-@Schema()
+@Schema({timestamps:true})
 export class Blog extends Document {
     @Prop()
-    titel:string;
+    title:string;
     @Prop()
     content:string;
 }
