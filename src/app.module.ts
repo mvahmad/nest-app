a285806e555cb42f209bba931f1edc3e013f80a2
mvahmad <kevetsahedrakh@hotmail.com>
2025-10-31 14:28:35 +0330

    feat : conect database to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BlogController } from './blog/blog.controller';
-import { BlogService } from './blog/blog.service';
+// import { BlogController } from './blog/blog.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [BlogModule],
+  imports: [
+    BlogModule,
+    MongooseModule.forRoot("mongodb://localhost:27017/nest-app")
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
