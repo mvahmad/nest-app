2d5e88f716e58618f92a3193f3ffea371a4e494b
mvahmad <kevetsahedrakh@hotmail.com>
2025-11-03 15:14:38 +0330

    feat : upload-file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { UploadFileDto } from './shared/dtos/upload-file.dto';
 //controlers are Task of directing all types of requests
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload-file')
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiBody({ type: UploadFileDto })
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    // 
+    return { filename: file.originalname };
   }
+
+  
 }
---
+++ src/shared/dtos/upload-file.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UploadFileDto {
+    @ApiProperty({
+        type:"string",
+        format:"binary",
+        required:true
+    })
+    file:any
+}
